461ead44cbd9e27eb12676926883dbb8d5996771
RatchBax <116721241+RatchBax@users.noreply.github.com>
2023-09-12 16:55:17 +0200

    Contrôlez la valeur des paramètres

--- submit_contact.php
+++ submit_contact.php
@@ -11,9 +11,8 @@
 </head>
 
 <body>
-    <?php if (!isset($_GET['email']) || !isset($_GET['message'])) {
-        echo "<h1> Erreur dans le formulaire.</h1>
-        <p>Vous n'avez pas renseigné tout les champs du formulaire.</p>";
+    <?php if ((!isset($_GET['email']) || !filter_var($_GET['email'], FILTER_VALIDATE_EMAIL)) || (!isset($_GET['message']) || empty($_GET['message']))) {
+        echo ('<h1> Il faut un email et un message valides pour soumettre le formulaire. </h1>');
         return;
     } ?>
 
