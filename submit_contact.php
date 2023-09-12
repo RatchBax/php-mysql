3901f8903037660f96dd472b9508499130919355
RatchBax <116721241+RatchBax@users.noreply.github.com>
2023-09-12 17:06:33 +0200

    Privilégiez la méthode post

--- submit_contact.php
+++ submit_contact.php
@@ -11,7 +11,7 @@
 </head>
 
 <body>
-    <?php if ((!isset($_GET['email']) || !filter_var($_GET['email'], FILTER_VALIDATE_EMAIL)) || (!isset($_GET['message']) || empty($_GET['message']))) {
+    <?php if ((!isset($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) || (!isset($_POST['message']) || empty($_POST['message']))) {
         echo ('<h1> Il faut un email et un message valides pour soumettre le formulaire. </h1>');
         return;
     } ?>
@@ -23,12 +23,12 @@
         <div class="card-body">
             <h5 class="card-title">Rappel de vos informations</h5>
             <p class="card-text">
-                <b>Email</b> : <?php $_GET['email'] ?>
+                <b>Email</b> : <?php echo $_POST['email'] ?>
 
             </p>
 
             <p class="card-text">
-                <b>Message</b> : <?php $_GET['message'] ?>
+                <b>Message</b> : <?php echo $_POST['message'] ?>
             </p>
         </div>
     </div>
