71fbd073dabb97ee1aa4080022afb334bd8883b6
RatchBax <116721241+RatchBax@users.noreply.github.com>
2023-09-12 17:22:41 +0200

    Sécurisez votre code

--- submit_contact.php
+++ submit_contact.php
@@ -23,12 +23,12 @@
         <div class="card-body">
             <h5 class="card-title">Rappel de vos informations</h5>
             <p class="card-text">
-                <b>Email</b> : <?php echo $_POST['email'] ?>
+                <b>Email</b> : <?php echo strip_tags($_POST['email']); ?>
 
             </p>
 
             <p class="card-text">
-                <b>Message</b> : <?php echo $_POST['message'] ?>
+                <b>Message</b> : <?php echo strip_tags($_POST['message']); ?>
             </p>
         </div>
     </div>
