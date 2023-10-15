85c94b8e0f92654fbc6eec32a2b822e22df2999a
RatchBax <116721241+RatchBax@users.noreply.github.com>
2023-10-15 11:47:43 +0200

    Améliorations esthétiques

--- src/model.php
+++ src/model.php
@@ -1,27 +1,26 @@
 <?php
     function getPosts() {
-        // Connexion à la base de données
+        // We connect to the database.
         try {
             $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
         }
         catch(Exception $e){
             die('Erreur : '.$e->getMessage());
         }
-        // On récupère les 5 derniers billets
+        // We retrieve the 5 last blog posts.
         $statement = $database->query(
-            "SELECT id, titre, contenu, DATE_FORMAT(date_creation, '%d/%m/%Y à %Hh%imin%ss') 
-            AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5"
+            "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') 
+            AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5"
         );
 
         $posts = []; 
         while ($row = $statement->fetch()) {
             $post = [
-                'title' => $row['titre'],
-                'content' => $row['contenu'],
-                'frenchCreationDate' => $row['date_creation_fr']
+                'title' => $row['title'],
+                'content' => $row['content'],
+                'frenchCreationDate' => $row['creation_date_fr']
             ]; 
             $posts[] = $post;
         }
         return $posts;
     }
-?>
--- templates/homepage.php
+++ templates/homepage.php
@@ -14,20 +14,15 @@
         ?>
         <div class="news">
            <h3>
-                <?php echo htmlspecialchars($post['title']); ?>
-                <em>le <?php echo $post['frenchCreationDate']; ?></em>
+                <?= htmlspecialchars($post['title']); ?>
+                <em>le <?= $post['frenchCreationDate']; ?></em>
            </h3>
            <p>
-                <?php
-                     // On affiche le contenu du billet
-                     echo nl2br(htmlspecialchars($post['content']));
-                ?>
-                <br />
+                <?= nl2br(htmlspecialchars($post['content'])); ?>
+                <br>
                 <em><a href="#">Commentaires</a></em>
            </p>
         </div>
-        <?php
-        } // Fin de la boucle des billets
-        ?>
+        <?php } ?>
    </body>
 </html>
