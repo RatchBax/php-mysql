c10f163e40cc739ab59274f5523f44b9cd855438
RatchBax <116721241+RatchBax@users.noreply.github.com>
2023-10-15 14:18:57 +0200

    Mettez à jour le modèle

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<!--
-    NOTE POUR LA CORRECTION
-
-    Ce commit contient encore des modifications pour la partie d'avant car je n'avais pas comprit qu'il fallait refaire
-    un autre fichier "post.php" à la racine du dossier de travail.
--->
-
-<?php    
-require('post.php');
+<?php
 require('src/model.php');
 $posts = getPosts();
 require('templates/homepage.php');
--- src/model.php
+++ src/model.php
@@ -1,12 +1,16 @@
 <?php
 
-function getPosts() {
+function dbConnect() {
     try {
         $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
     } catch(Exception $e) {
         die('Erreur : '.$e->getMessage());
     }
+    return $database;
+}
 
+function getPosts() {
+    $database = dbConnect();
     $statement = $database->query(
         "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts ORDER BY creation_date DESC LIMIT 0, 5"
     );
@@ -23,4 +27,45 @@ function getPosts() {
     }
 
     return $posts;
+}
+
+function getPost($identifier) {
+    $database = dbConnect();
+    $statement = $database->prepare(
+        "SELECT id, title, content,
+        DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss')
+        AS french_creation_date FROM posts WHERE id = ?"
+    );
+
+    $statement->execute([$identifier]);
+    $row = $statement->fetch();
+    $post = [
+        'title' => $row['title'],
+        'french_creation_date' => $row['french_creation_date'],
+        'content' => $row['content'],
+    ];
+
+    return $post;
+}
+
+function getComments($identifier) {
+    $database = dbConnect();
+    $statement = $database->prepare(
+        "SELECT id, author, comment,
+        DATE_FORMAT(comment_date, '%d/%m/%Y à %Hh%imin%ss')
+        AS french_creation_date FROM comments WHERE post_id = ?
+        ORDER BY comment_date DESC"
+    );
+    $statement->execute([$identifier]);
+    $comments = [];
+
+    while (($row = $statement->fetch())) {
+        $comment = [
+            'author' => $row['author'],
+            'french_creation_date' => $row['french_creation_date'],
+            'comment' => $row['comment'],
+        ];
+        $comments[] = $comment;
+    }
+    return $comments;
 }
