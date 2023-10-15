713810224f9c64f7187a26d2209470de5a6f5a63
RatchBax <116721241+RatchBax@users.noreply.github.com>
2023-10-15 13:43:50 +0200

    [TD 2] - Créez la vue et le contrôleur

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
-    require('src/model.php');
-    $posts = getPosts();
-    require('templates/homepage.php');
+    $post = [
+        'title' => 'Un faux titre.',
+        'french_creation_date' => '03/03/2022 à 12h14min42s',
+        'content' => "Le faux contenu de mon billet.\nC'est fantastique !",
+    ];
+
+    $comments = [
+        [
+            'author' => 'Un premier faux auteur',
+            'french_creation_date' => '03/03/2022 à 12h15min42s',
+            'comment' => 'Un faux commentaire.\n Le premier.',
+        ],
+        [
+            'author' => 'Un second faux auteur',
+            'french_creation_date' => '03/03/2022 à 12h16min42s',
+            'comment' => 'Un faux commentaire.\n Le second.',
+        ],
+    ];
+
+require('templates/post.php');
+require('src/model.php');
+require('templates/homepage.php');
+$posts = getPosts();
--- templates/homepage.php
+++ templates/homepage.php
@@ -4,25 +4,28 @@
         <meta charset="utf-8" />
         <title>Le blog de l'AVBN</title>
         <link href="style.css" rel="stylesheet" />
-   </head>
-   <body>
+    </head>
+
+    <body>
         <h1>Le super blog de l'AVBN !</h1>
         <p>Derniers billets du blog :</p>
 
         <?php
         foreach ($posts as $post) {
         ?>
-        <div class="news">
-           <h3>
-                <?= htmlspecialchars($post['title']); ?>
-                <em>le <?= $post['frenchCreationDate']; ?></em>
-           </h3>
-           <p>
-                <?= nl2br(htmlspecialchars($post['content'])); ?>
-                <br>
-                <em><a href="#">Commentaires</a></em>
-           </p>
-        </div>
-        <?php } ?>
-   </body>
+            <div class="news">
+                <h3>
+                    <?= htmlspecialchars($post['title']); ?>
+                    <em>le <?= $post['french_creation_date']; ?></em>
+                </h3>
+                <p>
+                    <?= nl2br(htmlspecialchars($post['content'])); ?>
+                    <br />
+                    <em><a href="#">Commentaires</a></em>
+                </p>
+            </div>
+        <?php
+        }
+        ?>
+    </body>
 </html>
