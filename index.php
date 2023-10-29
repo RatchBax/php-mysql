1a7adadb821d8c417df6c6c1c7945f6c1ba69fb7
RatchBax <116721241+RatchBax@users.noreply.github.com>
2023-10-29 13:14:22 +0100

    Ajoutez des commentaires

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require_once('src/controllers/add_comment.php');
 require_once('src/controllers/homepage.php');
 require_once('src/controllers/post.php');
 
@@ -7,6 +8,14 @@ if (isset($_GET['action']) && $_GET['action'] !== '') {
         if (isset($_GET['id']) && $_GET['id'] > 0) {
             $identifier = $_GET['id'];
             post($identifier);
+        } else {
+            echo 'Erreur : aucun identifiant de billet envoyé';
+            die;
+        }
+    } elseif ($_GET['action'] === 'addComment') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            $identifier = $_GET['id'];
+            addComment($identifier, $_POST);
         } else {
             echo 'Erreur : aucun identifiant de billet envoyé';
             die;
--- src/controllers/post.php
+++ src/controllers/post.php
@@ -1,5 +1,7 @@
 <?php
 require_once('src/model.php');
+require_once('src/model/comment.php');
+
 function post(string $identifier) {
     $post = getPost($identifier);
     $comments = getComments($identifier);
