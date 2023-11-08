a8cd2a22aa1dd79fa6e58ce7f9fb41928a6fe338
RatchBax <116721241+RatchBax@users.noreply.github.com>
2023-11-08 10:56:48 +0100

    Tirez parti de la composition

--- src/controllers/homepage.php
+++ src/controllers/homepage.php
@@ -1,11 +1,11 @@
 <?php
 
+require_once('src/lib/database.php');
 require_once('src/model/post.php');
 
-function homepage()
-{
+function homepage() {
     $postRepository = new PostRepository();
+    $postRepository->connection = new DatabaseConnection();
     $posts = $postRepository->getPosts();
-
     require('templates/homepage.php');
 }
--- src/controllers/post.php
+++ src/controllers/post.php
@@ -1,13 +1,15 @@
 <?php
 
-require_once('src/model/post.php');
+require_once('src/lib/database.php');
 require_once('src/model/comment.php');
+require_once('src/model/post.php');
 
-function post(string $identifier)
-{
+function post(string $identifier) {
     $postRepository = new PostRepository();
+    $comment = new CommentRepository();
+    $postRepository->connection = new DatabaseConnection();
+    $comment->connection = new DatabaseConnection();
     $post = $postRepository->getPost($identifier);
-    $comments = getComments($identifier);
-
+    $comments = $comment->getComments($identifier);
     require('templates/post.php');
 }
